448ef45ef05b3408b64783639a1bfb77425330ca
Peter Lind <petl@MacBook-Pro-2.local>
2018-05-17 13:23:24 +0200

    added delegate event

--- main.js
+++ main.js
@@ -3,20 +3,41 @@
 // create divs
 const app = document.querySelector("#app");
 
-const numberOfBubbles = 300;
+const numberOfBubbles = 10;
 
 for( let i=0; i < numberOfBubbles; i++ ) {
     const div = document.createElement("div");
     app.appendChild(div);
 }
 
-const divs = document.querySelectorAll("#app div");
+app.addEventListener("mouseout", clicked);
 
-divs.forEach(function(div){
-    //div.style.transform = `translate(50vw, 50vh)`
-    div.addEventListener("mouseenter", function(){
-        div.style.transform= `translate(${Math.random()*100}vw, ${Math.random()*100}vh)`;
-        div.style.backgroundColor = `hsl(${Math.random()*360}, ${Math.random()*100}%, 50%)`;
-        
-    });
-});
+
+function clicked( event ) {
+    console.log(event);
+    const target = event.target;
+
+    if( target.id === "app" ) {
+        // We leave the parent ...
+
+        const ourDiv = event.relatedTarget;
+        if( ourDiv != null && ourDiv.parentElement != null && ourDiv.parentElement.id === "app") {
+            randomMove(ourDiv);
+        }
+    }
+}
+
+function randomMove(ourDiv) {
+    ourDiv.style.transform= `translate(${Math.random()*100}vw, ${Math.random()*100}vh)`;
+    ourDiv.style.backgroundColor = `hsl(${Math.random()*360}, ${Math.random()*100}%, 50%)`;
+
+}
+
+function createNewBubble() {
+    const div = document.createElement("div");
+    app.appendChild(div);
+    randomMove(div);
+
+}
+
+setInterval( createNewBubble, 1000);
